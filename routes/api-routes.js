5f25cdc2b8a128f75510fdbfcb4a143c791bf7c8
Michael Eggers <mdeggers84@gmail.com>
2017-06-24 10:13:11 -0400

    :imp: removed ign srape

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 module.exports = function (app) {
-  app.get('/scrapePoly', function (req, res) {
+  app.get('/scrape', function (req, res) {
     request('https://www.polygon.com/', function (error, response, html) {
       var $ = cheerio.load(html);
       var result = [];
@@ -18,28 +18,6 @@ module.exports = function (app) {
       });
       console.log(result);
       res.json(result);
-    });
-  });
-
-  app.get('/scrapeIGN', function (req, res) {
-    request('http://www.ign.com/', function (error, response, html) {
-      var $ = cheerio.load(html);
-      var result = [];
-
-      $('.listElmnt').each(function (i, element) {
-        // var title = $(element).text();
-        // var link = $(element).find('a').attr('href');
-
-        var title = 'test';
-        var link = 'test';
-
-        result.push({
-          title: title,
-          link: link
-        });
-      });
-      console.log(result);
-      res.send(result);
     });
   });
 };
