639fe208041a9f383909435a3fa8f83fa7ce40df
Michael Eggers <mdeggers84@gmail.com>
2017-06-25 12:20:37 -0400

    index populates with title/links

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,19 @@
+var News = require('../models/News');
+var Comments = require('../models/Comments');
+
 module.exports = function (app) {
   app.get('/', function (req, res) {
-    res.render('index', { test: 'test' });
+    News.find({}, function (error, doc) {
+      if (error) {
+        console.log(error);
+      } else {
+        var hbsObject = {
+          doc: doc
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+      }
+    });
   });
 
   app.get('/saved', function (req, res) {
