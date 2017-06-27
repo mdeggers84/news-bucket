14c1f05792c8b26480db974a9507761dbf8e3bc7
Michael Eggers <mdeggers84@gmail.com>
2017-06-27 09:47:20 -0400

    setting up modal for comments

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  $('#commentModal').on('shown.bs.modal', function () {
+    $('#commentInput').focus();
+  });
+
   $(document).on('click', '.save-btn', function (event) {
     event.preventDefault();
     var newsID = $(this).data('id');
@@ -6,6 +10,15 @@ $(document).ready(function () {
     $.get('/api/news/' + newsID).done(function (data) {
       console.log('Saved Article', data);
       $.post('/api/saved/' + newsID, data);
+    });
+  });
+
+  $(document).on('click', '.comment-btn', function (event) {
+    event.preventDefault();
+    var newsID = $(this).data('id');
+
+    $.get('/api/saved/' + newsID).done(function (data) {
+
     });
   });
 
