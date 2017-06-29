a81eadb1cf2bbc4eda8a3d7ae37f1d3c4ffe208f
Michael Eggers <mdeggers84@gmail.com>
2017-06-29 13:19:17 -0400

    added SavedNews model

---
+++ models/SavedNews.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var savedNewsSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  body: String,
+  link: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Comments'
+  }]
+});
+
+var SavedNews = mongoose.model('SavedNews', savedNewsSchema);
+
+module.exports = SavedNews;
