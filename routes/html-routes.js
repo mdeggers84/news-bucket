ea73ecd08fc5d80374f770ac05fb1b96de1e9815
Michael Eggers <mdeggers84@gmail.com>
2017-06-29 18:19:50 -0400

    removed extra code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,3 @@
-var request = require('request');
-var cheerio = require('cheerio');
-
 var News = require('../models/News');
 var Comments = require('../models/Comments');
 var SavedNews = require('../models/SavedNews');
@@ -19,25 +16,6 @@ module.exports = function (app) {
       }
     });
   });
-
-  // app.get('/', function (req, res) {
-  //   request('https://www.polygon.com/', function (error, response, html) {
-  //     var $ = cheerio.load(html);
-  //     var hbsObject = {
-  //       docArr: []
-  //     };
-
-  //     $('div.c-compact-river__entry ').each(function (i, element) {
-  //       var doc = {};
-
-  //       doc.title = $(this).find('h2').find('a').text();
-  //       doc.link = $(this).find('h2').find('a').attr('href');
-
-  //       hbsObject.docArr.push(doc);
-  //     });
-  //     res.render('index', hbsObject);
-  //   });
-  // });
 
   app.get('/saved', function (req, res) {
     SavedNews.find({}).sort({ _id: 1 }).exec(function (error, doc) {
